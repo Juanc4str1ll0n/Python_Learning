4991f88fb97f548a5a5dc8fae476d03c6d1915a9
juan <juancastrillon868@gmail.com>
2025-07-28 19:31:24 -0500

    Metodos de arbol: Esta balanceado, altura, recorridos post, in y pre orden

--- Data_Structures/Arboles/ArbolBinario.py
+++ Data_Structures/Arboles/ArbolBinario.py
@@ -9,13 +9,15 @@ class Node:
 class BinaryTree:
     def __init__(self):
         self.raiz = None
+        self.size = 0
     
     def insertar(self, valor):
         if self.raiz == None:
             self.raiz = Node(valor)
+            self.size += 1
         else:
             self._insertarRecursivo(Nodo_actual = self.raiz, valor =valor)   #Empezamos desde la raiz
-    
+
     
     def _insertarRecursivo(self, Nodo_actual, valor):
         
@@ -23,14 +25,16 @@ class BinaryTree:
         if valor < Nodo_actual.valor:
             if Nodo_actual.izquierdo == None:
                 Nodo_actual.izquierdo = Node(valor)
+                self.size += 1
                 return
             else:
                 self._insertarRecursivo(Nodo_actual= Nodo_actual.izquierdo, valor= valor)
         
         #Insertar a la derecha (Es mayor)
         elif valor > Nodo_actual.valor:
-            if Nodo_actual.derecha == None:
-                Nodo_actual.derecha = Node(valor)
+            if Nodo_actual.derecho == None:
+                Nodo_actual.derecho = Node(valor)
+                self.size += 1
                 return
             else:
                 self._insertarRecursivo(Nodo_actual= Nodo_actual.derecho, valor= valor)
@@ -114,8 +118,8 @@ class BinaryTree:
     #                 actual = actual.izquierdo
     #-------------------------------------------
 
-    def size(self):
-        pass
+    def Peso(self):
+        return self.size
     
     def maximo(self):
         self._buscarMaximo(self.raiz)
@@ -130,32 +134,91 @@ class BinaryTree:
     
     def inorden(self):
         self._inordenRecursivo(self.raiz)
-    
+        print()
+        
     def _inordenRecursivo(self, nodo):
         if nodo is not None:
             self._inordenRecursivo(nodo.izquierdo)
-            print(nodo.valor, end="")
+            print(nodo.valor, end=" - ")
             self._inordenRecursivo(nodo.derecho)
-    
+        
+        
     def preorden(self):
-        pass
+        #Raiz - izq  - derecha
+        self._preordenRecursivo(self.raiz)
+        print()
     
-    def _preordenRecursivo(self):
-        pass
-    
+    def _preordenRecursivo(self, nodo):
+        if nodo is not None:
+            print(nodo.valor, "-",  end=" ")
+            self._preordenRecursivo(nodo.izquierdo)
+            self._preordenRecursivo(nodo.derecho)
+            
     def postorden(self):
+        self._postordenRecursivo(self.raiz)
+        print()
+        
+    def _postordenRecursivo(self, nodo):
+        if nodo is not None:
+            self._postordenRecursivo(nodo.izquierdo)
+            self._postordenRecursivo(nodo.derecho)
+            print(nodo.valor, end=" - ")
+            
+    def contarNodos(self):
+        return self.size
+    
+    def altura(self):
+        return self._altura_recursiva(self.raiz)
+    
+    def _altura_recursiva(self, nodo):
+        if nodo == None:
+            return 0
+        
+        izquierdo = self._altura_recursiva(nodo.izquierdo) 
+        derecho = self._altura_recursiva(nodo.derecho)
+
+        return 1 + max(izquierdo, derecho) 
+    
+    def es_balanceado(self):
+        return self._es_balanceado_recursivo(self.raiz)
+    
+    def _es_balanceado_recursivo(self, nodo):
+        if nodo is None:
+            return True
+        
+        altura_izquierda = self._altura_recursiva(nodo.izquierdo)
+        altura_derecha = self._altura_recursiva(nodo.derecho)
+        
+        diferencia = abs(altura_izquierda - altura_derecha)
+        
+        return (diferencia <= 1 and self._es_balanceado_recursivo(nodo.izquierdo) and self._es_balanceado_recursivo(nodo.derecho))
+    
+    def aListaOrdenada(self):
         pass
-    
-    def postordenRecursivo(self):
-        pass
-    
-    
-    
     
     
 arbolito = BinaryTree()
 
-arbolito.insertar(9)
+arbolito.insertar(50)
+arbolito.insertar(40)
+arbolito.insertar(30)
+arbolito.insertar(31)
+arbolito.insertar(20)
+arbolito.insertar(45)
+arbolito.insertar(42)
+arbolito.insertar(70)
+arbolito.insertar(69)
+arbolito.insertar(90)
 
 print(arbolito.buscar(90))
-    
+
+arbolito.inorden()
+arbolito.preorden()
+arbolito.postorden()
+
+# print(arbolito.altura())
+
+# print(arbolito.contarNodos())
+
+# print(arbolito.altura())
+print(arbolito.es_balanceado())
